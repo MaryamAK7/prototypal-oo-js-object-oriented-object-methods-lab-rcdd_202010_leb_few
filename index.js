af23e8ce837c7ce43d3d47e391a6aaabe85222a8
Maryam Abdel-karim <maryam.abdelkarim@hotmail.com>
2020-12-15 19:18:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,5 +18,5 @@ BoardMember.prototype.releasePressStatement(){
   return 'You will see great things from Scuber.'
 }
 BoardMember.prototype.sayHi(){
-  return '"Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.'
+  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 }
